21648482ac23f4b18d69763a02ba8e993998a44d
James Cool <jeffreysbrother@gmail.com>
2015-10-02 14:09:34 -0700

    removed content on original contact page

--- contact/index.php
+++ contact/index.php
@@ -7,8 +7,7 @@ include '../inc/head.php';
     </div>
     <div class="bottom_spacer"></div>
     <div class="content1">
-      <h1>CONTACT ME:</h1>You can reach me by email at <a href="mailto:tkray@tri-dynamic.com?subject=Hello%20There">tkray@planarform.com</a>.
-      <p>
+      
     </div>
 
 <?php
